f129012c6f980f220c070abb5018a8a9096c5898
Parminder Singh <parminder.s@flipkart.com>
2019-01-14 15:42:09 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'qf-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'questions-feed';
+}
